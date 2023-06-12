9acb68fec320e9fbb42fc4aaf377a67524a13f6f
vpwash <vpwash@gmail.com>
2023-06-11 19:01:06 -0500

    Added index page.

---
+++ app/index.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native';
+
+const LoginPage = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Everly's App</Text>
+    </View>
+  );
+};
+export default LoginPage;
