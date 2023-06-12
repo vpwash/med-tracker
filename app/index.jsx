dcbbbc18e348ae35a04c7a42fa71d71b68c17d4f
vpwash <77463429+vpwash@users.noreply.github.com>
2023-06-12 01:54:58 +0000

    Tailwind

--- app/index.jsx
+++ app/index.jsx
@@ -1,10 +1,17 @@
-import { View, Text } from 'react-native';
+import { Link } from 'expo-router'
+import { View, Text, TouchableOpacity } from 'react-native'
 
 const LoginPage = () => {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Everly's App</Text>
+    <View className='flex-1 items-center justify-center bg-white'>
+      <Link href={'/register'} asChild>
+        <TouchableOpacity>
+          <Text className='bg-slate-900 text-white py-5 px-10'>
+            Create Account
+          </Text>
+        </TouchableOpacity>
+      </Link>
     </View>
-  );
-};
-export default LoginPage;
+  )
+}
+export default LoginPage
---
+++ app/register.jsx
@@ -0,0 +1,15 @@
+import { View, Text, Pressable } from 'react-native'
+import React from 'react'
+
+const RegisterUser
+ = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Pressable>
+        <Text>Create Account</Text>
+      </Pressable>
+    </View>
+  )
+}
+
+export default RegisterUser
